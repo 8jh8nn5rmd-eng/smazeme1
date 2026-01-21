2b7ab5af804ade837d1368f38e55d7e61d14c8b2
Endrju82 <ondrej@hotarek.cz>
2026-01-21 12:20:07 +0100

    OH dokončení patičky a uprava diakritiky

--- index.py
+++ index.py
@@ -1,16 +1,18 @@
 #deklarace
 
-#hlavi4ka
+#hlavička
 
 #Telo
 
 #nadpis
 <h1>Vitejte na me strance!</h1>
 
-#pati4ka
+#patička
 Super design patičky
 Tady bude nadupaný obsah patičky.
 bla bla bla 
 
+#podpati4ka
+tady bude taky něco
 
 #konec
